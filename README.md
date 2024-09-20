599b7975e9fc8354dc79c440b0c1267dccc259a5
diosamuel <koding12sam@gmail.com>
2024-09-20 21:13:41 +0700

    main

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-# flask-guest-book
-
-## Overview
-
-This is a guest book wrapped into a Webapp using [Flask](http://flask.pocoo.org/) Micro Framework. The app is connected to a MySQL database hosted for free in [remotemysql](https://remotemysql.com/).
+# Projek Buku Tamu
 
 ## Dependencies
 
-```sudo pip install -r requirements.txt```
+```ip install -r requirements.txt```
 
 ## Usage
-
-Once dependencies are installed, just run this to see it in your browser. 
 
 ```python guestbook.py```
--- guestbook.py
+++ guestbook.py
@@ -1,33 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-# instanciating the app using the module name
 app = Flask(__name__)
 
-# configuring the db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql7320377:DBbYrSp2Y5@sql7.freemysqlhosting.net:3306/sql7320377'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/bukutamu'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-# each class in python will be mapped to a table in the db
-
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     comment = db.Column(db.String(1000))
 
-
 @app.route('/')
 def index():
     result = Comments.query.all()
     return render_template('index.html', result=result)
 
-
 @app.route('/sign')
 def sign():
     return render_template('sign.html')
-
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -40,6 +31,27 @@ def process():
 
     return redirect(url_for('index'))
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    comment_to_delete = Comments.query.get_or_404(id)
+    db.session.delete(comment_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>')
+def update(id):
+    comment_to_update = Comments.query.get_or_404(id)
+    return render_template('update.html', comment=comment_to_update)
+
+@app.route('/update_process/<int:id>', methods=['POST'])
+def update_process(id):
+    comment_to_update = Comments.query.get_or_404(id)
+    
+    comment_to_update.name = request.form['name']
+    comment_to_update.comment = request.form['comment']
+    
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
